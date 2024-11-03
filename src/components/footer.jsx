5f46242683b2c54ebe983c4789648c0237514904
NeshJesse <nehemiahjess@gmail.com>
2024-11-03 14:04:35 +0300

    location added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -21,9 +21,17 @@ const Footer = () => {
                 <p className='font-bold uppercase'>Contact Us or get in touch</p>
                 <p className='py-4'>Feel free to shoot us a message.</p>
                 <ul>
-                    <li>Email: </li>
-                    <li>Phone; </li>
+                    <li>Email: akornserviceslimited@gmail.com</li>
+                    <li>Phone; +254 789 374 217</li>
                 </ul>
+            </div>
+            <div className='col-span-2 pt-8 md:pt-2'>
+                <p className='font-bold uppercase'>Location</p>
+                <p className='py-4'>OPEN 24 HOURS MONDAY TO SATURDAY</p>
+                <p className='py-4'>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8522921600693!2d36.79984318885499!3d-1.260853799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f17e441790ac7%3A0x96af478219af41cd!2sKenrail%20Towers!5e0!3m2!1sen!2ske!4v1730631575472!5m2!1sen!2ske" width="300" height="200" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </p>
+                
             </div>
         </div>
 
